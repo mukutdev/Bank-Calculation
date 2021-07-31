a9655bea903dbb5b5fd954620f00d98f3d9b2c93
mukutdev <86616950+mukutdev@users.noreply.github.com>
2021-07-31 20:19:47 +0600

    Deposit and Withdraw system added

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,52 @@
-const loginBtn = document.getElementById('login');
-loginBtn.addEventListener('click', function(){
+//Login form hide js
 
-  const wrapper = document.getElementById('client-login-area'); 
-   wrapper.style.display  = 'none'; 
+const loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function () {
+
+  const wrapper = document.getElementById('client-login-area');
+  wrapper.style.display = 'none';
+  //transaction area show js  
+  const transactionArea = document.getElementById('transaction-area');
+  transactionArea.style.display = 'block';
 
 });
+
+//deposit button event handler
+
+
+const depositBtn = document.getElementById('deposit');
+depositBtn.addEventListener('click', function () {
+
+  const depositNumber = getInputNumber('depositAdd');
+
+  updateSpanText('currentBalance', depositNumber);
+  updateSpanText('currentDeposit', depositNumber);
+  document.getElementById('depositAdd').value = '';
+});
+
+//withdraw button event handler
+
+const withdrawBtn = document.getElementById('withdraw');
+withdrawBtn.addEventListener('click', function () {
+
+  const withdrawNumber = getInputNumber('withdrawAmount');
+  updateSpanText('currentWithdraw', withdrawNumber);
+  updateSpanText('currentBalance', -1 * withdrawNumber);
+  document.getElementById('withdrawAmount').value = '';
+
+})
+
+function getInputNumber(id) {
+  const amount = document.getElementById(id).value;
+  const amountNumber = parseFloat(amount);
+  return amountNumber;
+}
+
+// update span text function
+
+function updateSpanText(id, depositNumber) {
+  const current = document.getElementById(id).innerText;
+  const currentNumber = parseFloat(current);
+  const totalBalance = depositNumber + currentNumber;
+  document.getElementById(id).innerText = totalBalance;
+}
